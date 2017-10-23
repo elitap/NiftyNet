654cff8d2e7ee3a3369cd7360441d199de9e0559
Elias Tappeiner <elias.tappeiner@umit.at>
2017-10-23 10:27:40 +0200

    set qeuesize to one and threads to 2

--- tune_param.py
+++ tune_param.py
@@ -6,21 +6,26 @@ import sys
 ENDING = [".ini"]
 CONFIGS = "./config/tune_configs"
 
-DONE = ["full_e-3_96-1_dice_50k_1024s.ini", "quarter_e-5_48-8_gdsc_50k_1024s.ini"]
+DONE = ["quarter_e-5_48-8_gdsc_50k_1024s.ini", "half_e-3_96-1_dice_50k_1024s.ini",
+        "half_e-3_48-8_dice_50k_1024s.ini", "half_e-3_48-8_gdsc_50k_1024s.ini", "full_e-5_96-1_gdsc_50k_1024s.ini",
+        "half_e-4_48-8_dice_50k_1024s.ini", "half_e-3_96-1_gdsc_50k_1024s.ini"]
 
-MYPC = ["full_e-3_96-1_dice_50k_1024s.ini",
+#
+#"half_e-3_96-1_dice_50k_1024s.ini",
+#"half_e-3_48-8_dice_50k_1024s.ini",
+#"half_e-3_48-8_gdsc_50k_1024s.ini",
+#"full_e-5_96-1_gdsc_50k_1024s.ini",
+#"full_e-5_96-1_dice_50k_1024s.ini",
+#"half_e-4_48-8_dice_50k_1024s.ini",
+#"half_e-3_96-1_gdsc_50k_1024s.ini", not finished
+MYPC = ["full_e-5_96-1_dice_50k_1024s.ini"
+        "full_e-3_96-1_dice_50k_1024s.ini",
         "full_e-3_96-1_gdsc_50k_1024s.ini",
         "full_e-4_96-1_dice_50k_1024s.ini",
         "full_e-4_96-1_gdsc_50k_1024s.ini",
-        "full_e-5_96-1_dice_50k_1024s.ini",
-        "full_e-5_96-1_gdsc_50k_1024s.ini",
-        "half_e-3_48-8_dice_50k_1024s.ini",
-        "half_e-3_48-8_gdsc_50k_1024s.ini",
-        "half_e-3_96-1_dice_50k_1024s.ini",
-        "half_e-3_96-1_gdsc_50k_1024s.ini",
-        "half_e-4_48-8_dice_50k_1024s.ini",
-        "half_e-4_48-8_gdsc_50k_1024s.ini"]
+        "half_e-4_48-8_gdsc_50k_1024s.ini"] #running
 
+#"quarter_e-5_48-8_gdsc_50k_1024s.ini" not fully done
 OTHERPC = ["half_e-4_96-1_dice_50k_1024s.ini",
             "half_e-4_96-1_gdsc_50k_1024s.ini",
             "half_e-5_48-8_dice_50k_1024s.ini",
@@ -31,8 +36,7 @@ OTHERPC = ["half_e-4_96-1_dice_50k_1024s.ini",
             "quarter_e-3_48-8_gdsc_50k_1024s.ini",
             "quarter_e-4_48-8_dice_50k_1024s.ini",
             "quarter_e-4_48-8_gdsc_50k_1024s.ini",
-            "quarter_e-5_48-8_dice_50k_1024s.ini",
-            "quarter_e-5_48-8_gdsc_50k_1024s.ini"]
+            "quarter_e-5_48-8_dice_50k_1024s.ini"]
 
 
 
@@ -61,6 +65,7 @@ def run_tests(gpu, pc):
                 fullfile = os.path.join(CONFIGS, file)
 
                 cmd = "python net_segment.py train -c " + fullfile + " --cuda_devices " + str(gpu)
+                #print file, file in DONE, (pc == 0 and file in MYPC), (pc == 1 and file in OTHERPC)
                 if file not in DONE and ((pc == 0 and file in MYPC) or (pc == 1 and file in OTHERPC)):
                     if gpu == 0 and "dice" in file:
                         exec_niftynet(cmd, fptr)
