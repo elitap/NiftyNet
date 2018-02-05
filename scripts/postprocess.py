66fc31b3832093fa82d07def94338843fb172f1c
Elias Tappeiner <elias.tappeiner@gmx.at>
2018-02-05 10:44:09 +0100

    postproc labelwise

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -2,11 +2,41 @@ import argparse
 import SimpleITK as sitk
 import numpy as np
 import os
+from defs import LABELS
 
 POST_PROC_RES_DIR = "postprocess"
 
+def filter_using_coarse_out(res_itk, filter_itk, dilation_radius):
+    res_np = sitk.GetArrayFromImage(res_itk).astype(np.uint8)
+    filter_np = sitk.GetArrayFromImage(filter_itk).astype(np.uint8)
+    for key, value in enumerate(LABELS):
+        filter_cp = filter_np.copy()
+        filter_cp[filter_cp != value] = 0
+        filter_cp[filter_cp == value] = 1
 
-def postproc(resdir, postprocdir, forgrounddir, foreground_filter, dilation_radius):
+        filter_itk = sitk.GetImageFromArray(filter_cp)
+        dilation = sitk.BinaryDilateImageFilter()
+        dilation.SetKernelType(sitk.sitkBox)
+        dilation.SetKernelRadius(dilation_radius)
+        dilation.SetForegroundValue(1)
+        dilation.SetBackgroundValue(0)
+        filter_itk = dilation.Execute(filter_itk)
+
+        filter_cp = sitk.GetArrayFromImage(filter_itk)
+
+        res_np[(filter_cp == 0) & (res_np == value)] = 0
+
+    new_res_itk = sitk.GetImageFromArray(res_np)
+    new_res_itk.SetSpacing(res_itk.GetSpacing())
+    new_res_itk.SetOrigin(res_itk.GetOrigin())
+    new_res_itk.SetDirection(res_itk.GetDirection())
+
+    return new_res_itk
+
+
+
+
+def postproc(resdir, postprocdir, filter_dir, foreground_filter, dilation_radius):
     if not os.path.exists(postprocdir):
         os.mkdir(postprocdir)
     for res_file in os.listdir(resdir):
@@ -14,42 +44,48 @@ def postproc(resdir, postprocdir, forgrounddir, foreground_filter, dilation_radi
         if os.path.isfile(full_file):
             id = res_file[0:9]
             
-            for foreground in os.listdir(forgrounddir):
-                full_foreground = os.path.join(forgrounddir, foreground)
-                if os.path.isfile(full_foreground) and id in foreground and foreground_filter in foreground:
+            for filter in os.listdir(filter_dir):
+                full_filter = os.path.join(filter_dir, filter)
+                if os.path.isfile(full_filter) and id in filter and foreground_filter in filter:
                     
-                    foreground_itk = sitk.ReadImage(full_foreground)
+                    filter_itk = sitk.ReadImage(full_filter)
                     res_itk = sitk.ReadImage(full_file)
-                    np.testing.assert_almost_equal(foreground_itk.GetSpacing(), res_itk.GetSpacing(), 5,
+                    np.testing.assert_almost_equal(filter_itk.GetSpacing(), res_itk.GetSpacing(), 5,
                                                    "Spacing dimension does not match")
-                    np.testing.assert_almost_equal(foreground_itk.GetSize(), res_itk.GetSize(), 5,
+                    np.testing.assert_almost_equal(filter_itk.GetSize(), res_itk.GetSize(), 5,
                                                    "Size dimension does not match")
 
-
-                    cast_img_filter = sitk.CastImageFilter()
-                    cast_img_filter.SetOutputPixelType(sitk.sitkUInt8)
-                    foreground_itk = cast_img_filter.Execute(foreground_itk)
-
-                    dilation = sitk.BinaryDilateImageFilter()
-                    dilation.SetKernelType(sitk.sitkBox)
-                    dilation.SetKernelRadius(dilation_radius)
-                    dilation.SetForegroundValue(1)
-                    dilation.SetBackgroundValue(0)
-                    foreground_itk = dilation.Execute(foreground_itk)
-
-                    foreground_np = sitk.GetArrayFromImage(foreground_itk)
-                    res_np = sitk.GetArrayFromImage(res_itk)
-
-                    res_np[foreground_np == 0] = 0
-
-                    new_res_itk = sitk.GetImageFromArray(res_np)
-                    new_res_itk.SetSpacing(res_itk.GetSpacing())
-                    new_res_itk.SetOrigin(res_itk.GetOrigin())
-                    new_res_itk.SetDirection(res_itk.GetDirection())
+                    new_res_itk = old_postproc(dilation_radius, filter_itk, postprocdir, res_file, res_itk)
 
                     new_file = os.path.join(postprocdir, res_file[0:9] + "_out_postproc.nii.gz")
                     print new_file
                     sitk.WriteImage(new_res_itk, new_file)
+
+
+
+
+def old_postproc(dilation_radius, filter_itk, res_itk):
+
+    cast_img_filter = sitk.CastImageFilter()
+    cast_img_filter.SetOutputPixelType(sitk.sitkUInt8)
+    filter_itk = cast_img_filter.Execute(filter_itk)
+    
+    dilation = sitk.BinaryDilateImageFilter()
+    dilation.SetKernelType(sitk.sitkBox)
+    dilation.SetKernelRadius(dilation_radius)
+    dilation.SetForegroundValue(1)
+    dilation.SetBackgroundValue(0)
+    filter_itk = dilation.Execute(filter_itk)
+    foreground_np = sitk.GetArrayFromImage(filter_itk)
+    res_np = sitk.GetArrayFromImage(res_itk)
+    res_np[foreground_np == 0] = 0
+
+    new_res_itk = sitk.GetImageFromArray(res_np)
+    new_res_itk.SetSpacing(res_itk.GetSpacing())
+    new_res_itk.SetOrigin(res_itk.GetOrigin())
+    new_res_itk.SetDirection(res_itk.GetDirection())
+
+    return new_res_itk
 
 
 if __name__ == "__main__":
@@ -63,11 +99,11 @@ if __name__ == "__main__":
                         required=True,
                         help="directory the results are written to"
                         )
-    parser.add_argument('--foreground',
+    parser.add_argument('--coarse_stage_out',
                         required=True,
                         help="result dir containing the filter (coarse stage results)"
                         )
-    parser.add_argument('--foreground_filter',
+    parser.add_argument('--out_filter',
                         required=False,
                         default='foreground',
                         help='filter for the files in the foreground dir'
